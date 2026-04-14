58bf5188406d2d09589744a149259e9ec8f99b9c
Takahiro Joukou <takahiro.joukou@gmail.com>
2026-04-14 11:32:32 +0900

    feat: EmployeeListResponseにJSON非NULLフィルタリングを追加

--- src/main/java/com/example/employeeapi/dto/EmployeeListResponse.java
+++ src/main/java/com/example/employeeapi/dto/EmployeeListResponse.java
@@ -1,9 +1,11 @@
 package com.example.employeeapi.dto;
 
 import com.example.employeeapi.model.Employee;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EmployeeListResponse {
 
     private List<Employee> employees;
